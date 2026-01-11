d8d046f6ef50a79ae85718fb76562b7d248c0592
bransoned <bransonmb@proton.me>
2026-01-10 21:00:28 -0500

    Add basic settings file for constants

---
+++ octo-fiesta/Models/Settings/SquidWTFSettings.cs
@@ -0,0 +1,17 @@
+namespace octo_fiesta.Models.Settings;
+
+/// <summary>
+/// Configuration for the SquidWTF downloader and metadata service
+/// </summary>
+public class SquidWTFSettings
+{
+    /// <summary>
+	/// No user auth should be needed for this site.
+	/// </summary>
+    
+    /// <summary>
+    /// Preferred audio quality: FLAC, MP3_320, MP3_128
+    /// If not specified or unavailable, the highest available quality will be used.
+    /// </summary>
+    public string? Quality { get; set; }	
+}
