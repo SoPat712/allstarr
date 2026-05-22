7985070b78ba95d4d1009bef7a691c18ac1d01f4
Josh Patra <joshpatra12@gmail.com>
2026-05-22 19:57:43 -0400

    v2.0.2-beta.1: fix SquidWTF nullable metadata warning

--- allstarr/AppVersion.cs
+++ allstarr/AppVersion.cs
@@ -9,5 +9,5 @@ public static class AppVersion
     /// <summary>
     /// Current application version.
     /// </summary>
-    public const string Version = "2.0.1";
+    public const string Version = "2.0.2";
 }
