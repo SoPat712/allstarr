e160a98fc0fb2b40ac4ae615d6a3d2248ccdec7e
Josh Patra <joshpatra12@gmail.com>
2026-08-20 14:08:45 -0400

    chore(webui): allow Impeccable live mode in development

--- webui/svelte.config.js
+++ webui/svelte.config.js
@@ -1,5 +1,9 @@
 import adapter from "@sveltejs/adapter-static";
 
+// Dev-only allowance so Impeccable live mode can load. Guarded by NODE_ENV.
+const __impeccableLiveDev =
+  process.env.NODE_ENV === "development" ? ["http://localhost:8400"] : [];
+
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
   kit: {
@@ -17,12 +21,12 @@ const config = {
       directives: {
         "default-src": ["self"],
         "base-uri": ["self"],
-        "connect-src": ["self"],
+        "connect-src": ["self", ...__impeccableLiveDev],
         "font-src": ["self"],
         "form-action": ["self"],
         "img-src": ["self", "data:", "blob:"],
         "object-src": ["none"],
-        "script-src": ["self"],
+        "script-src": ["self", ...__impeccableLiveDev],
         "style-src": ["self", "unsafe-inline"],
       },
     },
